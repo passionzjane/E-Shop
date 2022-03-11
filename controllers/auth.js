b28339e1841b68982775351af02b92c524faf840
Jane Stack <57009779+passionzjane@users.noreply.github.com>
2022-03-11 08:20:41 -0800

    Using the session middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,19 @@
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req
-  .get('cookie')
-  .split('=')[1];
+  // const isLoggedIn = req
+  // .get('cookie')
+  // .split('=')[1];
+  console.log(req.session.isLoggedIn)
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoggedIn
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true; Secure')
+  req.session.isLoggedIn = true
   res.redirect('/')
 };
 
