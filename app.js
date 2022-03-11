7e495ad05fdea00a433669357a4603f6f71d4473
Jane Stack <57009779+passionzjane@users.noreply.github.com>
2022-03-11 09:17:48 -0800

    using mongodb to store sessions.

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session)
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb://localhost:27017/shoppApp'
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -20,7 +27,11 @@ const authRoutes = require("./routes/auth");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  session({secret: 'my secret', resave: false, saveUninitialized: false})
+  session({
+   secret: 'my secret',
+  resave: false, 
+  saveUninitialized: false,
+  store: store})
   )
 
 app.use((req, res, next) => {
@@ -41,7 +52,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 mongoose
   .connect(
-    'mongodb://localhost:27017/shoppApp',
+    MONGODB_URI,
     { useUnifiedTopology: true, 
       useNewUrlParser: true
     }
