b59e7aef6493e72a18ecd5dcef828cba341c4bf6
Jane Stack <57009779+passionzjane@users.noreply.github.com>
2022-03-15 07:52:37 -0700

    A bit of improvement improvement on hashing

--- controllers/auth.js
+++ controllers/auth.js
@@ -43,19 +43,21 @@ exports.postSignup = (req, res, next) => {
       if (userDoc) {
         return res.redirect('/signup')
       }
-      return bcrypt.hash(password, 12)
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then((result) => {
+          res.redirect("/login");
+        });
     })
-    .then((hashedPassword) => {
-      const user = new User({
-        email: email,
-        password: hashedPassword,
-        cart: { items: [] },
-      });
-      return user.save();
-    })
-    .then(result => {
-     res.redirect('/login')
-    })
+   
     .catch(err => {
       console.log(err)
   });
