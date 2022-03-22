f923bcbdeabcca855d249b64406f85e4fb08f870
Jane Stack <57009779+passionzjane@users.noreply.github.com>
2022-03-22 06:19:33 -0700

    Handling multipart Form data

--- routes/admin.js
+++ routes/admin.js
@@ -18,10 +18,14 @@ router.get("/products", isAuth, adminController.getProducts);
 router.post(
   "/add-product",
   [
-    body("title").isString().isLength({ min: 3 }).trim(),
-    body("imageUrl").isURL(),
+    body("title")
+    .isString()
+    .isLength({ min: 3 })
+    .trim(),
     body("price").isFloat(),
-    body("description").isLength({ min: 5, max: 400 }).trim(),
+    body("description")
+    .isLength({ min: 5, max: 400 })
+    .trim(),
   ],
   isAuth,
   adminController.postAddProduct
@@ -32,10 +36,14 @@ router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
 router.post(
   "/edit-product",
   [
-    body("title").isString().isLength({ min: 3 }).trim(),
-    body("imageUrl").isURL(),
+    body("title")
+    .isString()
+    .isLength({ min: 3 })
+    .trim(),
     body("price").isFloat(),
-    body("description").isLength({ min: 5, max: 400 }).trim(),
+    body("description")
+    .isLength({ min: 5, max: 400 })
+    .trim(),
   ],
   isAuth,
   adminController.postEditProduct
