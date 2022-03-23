2827459067ae4ec8a154191262537b5967d8ab7f
Jane Stack <57009779+passionzjane@users.noreply.github.com>
2022-03-22 19:32:18 -0700

    Storing file data in the database.

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,10 +15,24 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.file;
+  const image = req.file;
   const price = req.body.price;
   const description = req.body.description;
-  console.log(imageUrl)
+  if (!image) {
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Add Product",
+      path: "/admin/edit-product",
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        price: price,
+        description: description,
+      }, 
+      errorMessage: 'Attached file is not an image',
+      validationErrors: []
+    });
+  }
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -33,11 +47,13 @@ exports.postAddProduct = (req, res, next) => {
         imageUrl: imageUrl,
         price: price,
         description: description,
-      },
+      }, 
       errorMessage: errors.array()[0].msg,
       validationErrors: errors.array(),
     });
   }
+
+  const imageUrl = image.path;
 
   const product = new Product({
     title: title,
@@ -88,7 +104,7 @@ exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
   const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
+  const image = req.file;
   const updatedDesc = req.body.description;
 
   const errors = validationResult(req);
@@ -101,7 +117,6 @@ exports.postEditProduct = (req, res, next) => {
       hasError: true,
       product: {
         title: updatedTitle,
-        imageUrl: updatedImageUrl,
         price: updatedPrice,
         description: updatedDesc,
         _id: prodId,
@@ -119,7 +134,9 @@ exports.postEditProduct = (req, res, next) => {
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
+      if (image) {
+        product.imageUrl = image.path;
+      }
       return product.save().then((result) => {
         console.log("UPDATED PRODUCT!");
         res.redirect("/admin/products");
@@ -131,6 +148,57 @@ exports.postEditProduct = (req, res, next) => {
       return next(error);
     });
 };
+
+
+
+// exports.postEditProduct = (req, res, next) => {
+//   const prodId = req.body.productId;
+//   const updatedTitle = req.body.title;
+//   const updatedPrice = req.body.price;
+//   const image = req.file;
+//   const updatedDesc = req.body.description;
+
+//   const errors = validationResult(req);
+
+//   if (!errors.isEmpty()) {
+//     return res.status(422).render("admin/edit-product", {
+//       pageTitle: "Edit Product",
+//       path: "/admin/edit-product",
+//       editing: true,
+//       hasError: true,
+//       product: {
+//         title: updatedTitle,
+//         price: updatedPrice,
+//         description: updatedDesc,
+//         _id: prodId,
+//       },
+//       errorMessage: errors.array()[0].msg,
+//       validationErrors: errors.array(),
+//     });
+//   }
+
+//   Product.findById(prodId)
+//     .then((product) => {
+//       if (product.userId.toString() !== req.user._id.toString()) {
+//         return res.redirect("/");
+//       }
+//       product.title = updatedTitle;
+//       product.price = updatedPrice;
+//       product.description = updatedDesc;
+//       if (image) {
+//         product.imageUrl = image.path;
+//       }
+//       return product.save().then((result) => {
+//         console.log("UPDATED PRODUCT!");
+//         res.redirect("/admin/products");
+//       });
+//     })
+//     .catch((err) => {
+//       const error = new Error(err);
+//       error.httpStatusCode = 500;
+//       return next(error);
+//     });
+// };
 
 exports.getProducts = (req, res, next) => {
   Product.find({ userId: req.user._id })
