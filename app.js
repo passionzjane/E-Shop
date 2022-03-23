d512ef7fc2f59f89dc0d6fa4933f493161463c1f
Jane Stack <57009779+passionzjane@users.noreply.github.com>
2022-03-22 19:55:00 -0700

    serving images statically

--- app.js
+++ app.js
@@ -58,6 +58,8 @@ app.use(
   multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
 );
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, "images")));
+
 app.use(
   session({
    secret: 'my secret',
