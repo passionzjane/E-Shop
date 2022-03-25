bac18c2d4861ad9672481a3a618d716c81362464
Jane Stack <57009779+passionzjane@users.noreply.github.com>
2022-03-25 05:06:10 -0700

    generating pdf files with Order Data

--- controllers/shop.js
+++ controllers/shop.js
@@ -172,7 +172,24 @@ exports.getInvoice = (req, res, next) => {
     pdfDoc.pipe(fs.createWriteStream(invoicePath ));
     pdfDoc.pipe(res);
 
-    pdfDoc.text('Hello world!'); 
+    pdfDoc.fontSize(26).text('Invoice', {
+      underline: true
+    }); 
+    pdfDoc.text('--------------------------------');
+    let totalPrice = 0
+    order.products.forEach(prod => {
+      totalPrice += prod.quantity * prod.product.price
+      pdfDoc.fontSize(14).text(
+        prod.product.title +
+        ' = ' +
+        prod.quantity +
+        ' X ' +
+        '$' +
+        prod.product.price
+      )
+    })
+    pdfDoc.text('------------')
+    pdfDoc.fontSize(20).text('Total Price: $' + totalPrice)
 
     pdfDoc.end();
     // fs.readFile(invoicePath, (err, data) => {
