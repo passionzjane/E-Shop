b7a1a637e31a910d24f7f78afde438d4d79967d8
Jane Stack <57009779+passionzjane@users.noreply.github.com>
2022-03-25 12:31:18 -0700

    Manipulating the DOM

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,6 +2,8 @@
     const prodId = btn.parentNode.querySelector('[name=productId]').value;
     const csrf = btn.parentNode.querySelector("[name=_csrf]").value;
 
+    const productElement = btn.closest('article');
+
     fetch('/admin/product/' + prodId, {
         method: 'DELETE',
         headers: {
@@ -9,7 +11,11 @@
         }
     })
     .then(result => {
-        console.log(result);
+        return result.json();
+    })
+    .then(data => {
+        console.log(data);
+        productElement.parentNode.removeChild(productElement)
     })
     .catch(err => {
         console.log(err);
