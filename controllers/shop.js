66d63a15cd733041371148770e4a0f99db0ced7d
Jane Stack <57009779+passionzjane@users.noreply.github.com>
2022-03-25 04:07:52 -0700

    using PDFKit to generate PDF

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path')
 
+const PDFDocument = require('pdfkit')
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 const User = require("../models/user");
@@ -161,6 +163,18 @@ exports.getInvoice = (req, res, next) => {
     }
     const invoiceName = "invoice-" + orderId + ".pdf";
     const invoicePath = path.join("data", "invoices", invoiceName);
+    
+    const pdfDoc = new PDFDocument();
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader("Content-Disposition",
+     'inline: filename="' + invoiceName + '"'
+     );
+    pdfDoc.pipe(fs.createWriteStream(invoicePath ));
+    pdfDoc.pipe(res);
+
+    pdfDoc.text('Hello world!'); 
+
+    pdfDoc.end();
     // fs.readFile(invoicePath, (err, data) => {
     //   if (err) {
     //     return next(err);
@@ -172,14 +186,10 @@ exports.getInvoice = (req, res, next) => {
     //   );
     //   res.send(data);
     // });
-    const file = fs.createReadStream(invoicePath);
-    res.setHeader("Content-Type", "application/pdf");
-    res.setHeader(
-      "Content-Disposition",
-      'inline; filename="' + invoiceName + '"'
-    );
-    file.pipe(res)
+    // const file = fs.createReadStream(invoicePath);
+    
+    // file.pipe(res)
   })
   .catch(err => next(err));
   
-}
+} 
