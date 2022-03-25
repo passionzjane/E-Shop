0dcaf6d885191ddeb00627ee92fbbf0a515d5e3a
Jane Stack <57009779+passionzjane@users.noreply.github.com>
2022-03-25 11:58:44 -0700

    Sending and handling backend request

--- controllers/admin.js
+++ controllers/admin.js
@@ -221,24 +221,21 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
-  .then(product => {
-    if (!product) {
-      return next(new Error('Product not found.'))
-    }
-    fileHelper.deleteFile(product.imageUrl);
-    return Product.deleteOne({ _id: prodId, userId: req.user._id });
-  })
-  .then(() => {
-    console.log("DESTROYED PRODUCT");
-    res.redirect("/admin/products");
-  })
-  .catch((err) => {
-    console.log(err)
-    const error = new Error(err);
-    error.httpStatusCode = 500;
-    return next(error);
-  }); 
+    .then((product) => {
+      if (!product) {
+        return next(new Error("Product not found."));
+      }
+      fileHelper.deleteFile(product.imageUrl);
+      return Product.deleteOne({ _id: prodId, userId: req.user._id });
+    })
+    .then(() => {
+      console.log("DESTROYED PRODUCT");
+      res.status(200).json({ message: 'success!'});
+    })
+    .catch((err) => {
+     res.status(500).json({ message: 'Deleting product failed.'})
+    });
 };
