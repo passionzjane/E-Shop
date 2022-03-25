ac0a9ec19d5b9b07e08a5427fb0680b708529e2e
Jane Stack <57009779+passionzjane@users.noreply.github.com>
2022-03-25 07:23:21 -0700

    Retrieving a chunk of data

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,8 +6,11 @@ const PDFDocument = require('pdfkit')
 const Product = require('../models/product');
 const Order = require('../models/order');
 const User = require("../models/user");
+
+
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
-  
   Product.find()
     .then((products) => {
       console.log(products);
@@ -42,10 +45,12 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  console.log(req.user);
+const page = req.query.page;
+
   Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE)
     .then((products) => {
-  
       res.render("shop/index", {
         prods: products,
         pageTitle: "Shop",
