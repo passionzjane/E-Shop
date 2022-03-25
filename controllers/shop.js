6c2696b091a6a76d289ea2b91f0f668688bc0e5e
Jane Stack <57009779+passionzjane@users.noreply.github.com>
2022-03-25 07:42:29 -0700

    Preparing pagination data on the server

--- controllers/shop.js
+++ controllers/shop.js
@@ -46,22 +46,34 @@ exports.getProduct = (req, res, next) => {
 
 exports.getIndex = (req, res, next) => {
 const page = req.query.page;
+let totalItems
 
-  Product.find()
+Product.find()
+.countDocument()
+.then(numProducts => {
+  totalItems = numProducts;
+  return Product.find()
     .skip((page - 1) * ITEMS_PER_PAGE)
     .limit(ITEMS_PER_PAGE)
-    .then((products) => {
-      res.render("shop/index", {
-        prods: products,
-        pageTitle: "Shop",
-        path: "/",
-      });
-    })
-    .catch((err) => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+})
+  .then((products) => {
+    res.render("shop/index", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+      totalProducts: totalItems,
+      hasNextpage: ITEMS_PER_PAGE * page < totalItems,
+      hasPreviousPage: page > 1,
+      nextPage: page + 1,
+      previousPage: page - 1,
+      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
     });
+  })
+  .catch((err) => {
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    return next(error);
+  });
 };
 
 exports.getCart = (req, res, next) => {
